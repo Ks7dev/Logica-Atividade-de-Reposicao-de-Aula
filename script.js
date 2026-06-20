d35b7e61630678b1117b73361bb2e2611fa18301
Ks7dev <developer.ks7@gmail.com>
2026-06-19 23:34:10 -0300

    coleta de seletores do front para o back

---
+++ script.js
@@ -0,0 +1,16 @@
+let idAtual = 1;
+
+const funcionarioNome = document.querySelector("#funcionario-nome");
+const funcionarioCargo = document.querySelector("#funcionario-cargo");
+const funcionarioDepto = document.querySelector("#funcionario-depto");
+const funcionarioEmail = document.querySelector("#funcionario-email");
+const funcionarioId = document.querySelector("#funcionario-id");
+const funcionarioImagem = document.querySelector("#img-funcionario");
+const campoId = document.querySelector("#campo-busca");
+
+const botaoAlterar = document.querySelector("#botao-alterar");
+const botaoProximo = document.querySelector("#butao-proximo");
+const botaoAnterior = document.querySelector("#butao-anterior");
+
+const dummyurl = `https://dummyjson.com/users/`
+const randourl = `https://randomuser.me/api/?gender=`
